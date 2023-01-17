a6a8ae8d078c8f7091e2d2ad674985015b9c0aa9
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-01-18 01:50:14 +0600

    implement design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
 function App() {
+  const formHandler = (event) => {
+    event.preventDefault();
+    console.log(event.target)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <div className='mt-5'>
+          <h2 className='text-center my-5 bg-primary text-white p-3 border border-4 rounded border-success'>Node Mailer form</h2>
+          <Form onSubmit={formHandler} className='border border-3 border-success rounded p-5'>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control name='email' type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Phone</Form.Label>
+                <Form.Control name='phone'  type="tel" placeholder="Phone" />
+              </Form.Group>
+            </Row>
+
+            <Form.Group className="mb-3" controlId="formGridAddress1">
+              <Form.Label>Address</Form.Label>
+              <Form.Control  name='address'  placeholder="1234 Main St" />
+            </Form.Group>
+
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridCity">
+                <Form.Label>City</Form.Label>
+                <Form.Control placeholder='city' />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridZip">
+                <Form.Label>Zip</Form.Label>
+                <Form.Control  placeholder='zip' />
+              </Form.Group>
+            </Row>
+
+            <Button variant="primary" type="submit">
+              Send
+            </Button>
+          </Form>
+        </div>
+      </Container>
+
     </div>
   );
 }
